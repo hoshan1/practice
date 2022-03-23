625b44f87af43b4ab3a0882d8413229ee10ca430
hoshan1 <hosanzx@naver.com>
2022-03-23 23:18:23 +0900

    first commit

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log("hello");
+const name="Mike"
+const message=`My name is ${name}`;
+const message1="My name is ${name}";
+console.log(message)
+console.log(message1)
+const age =30;
+const PI=3.14;
+console.log(name=='mike')
+console.log(age<40)
+const nam = prompt("이름을 입력하세요. ");
+alert("환영합니다," + nam + "님");
